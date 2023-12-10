75bf6c700911de74d70b9ac4a4fb1b853e45b7fa
manuel1492 <manuelsandoval150217@gmail.com>
2023-12-10 14:50:31 -0600

    Primer commit

---
+++ plugins/grupo-tagall.js
@@ -0,0 +1,25 @@
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command}) => {
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn);
+      throw false;
+    }
+    const pesan = args.join` `;
+    const oi = `𝐌𝐎𝐓𝐈𝐕𝐎: ${pesan}`;
+    let teks = `🎅 𝐋𝐋𝐀𝐌𝐀𝐍𝐃𝐎 𝐀𝐋 𝐆𝐑𝐔𝐏𝐎 🎅\n\n ${oi}\n\n🎄 𝐌𝐄𝐍𝐂𝐈𝐎𝐍𝐄𝐒 🎄 \n`;
+    for (const mem of participants) {
+      teks += ` 🦌 @${mem.id.split('@')[0]}\n`;
+    }
+  
+    
+    teks += ` 
+    
+    𝐌𝐀𝐍𝐔𝐁𝐎𝐓\n\n`;
+    conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+  };
+  handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+  handler.tags = ['group'];
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+  handler.admin = true;
+  handler.group = true;
+  export default handler;
+  
